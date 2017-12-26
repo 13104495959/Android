d81fc3408b1408e3fb974b32a7740baf663c884e
宫赫 <gonghe0555@gmail.com>
2017-12-26 18:05:12 +0900

    1	增加BottomNavigationViewEx框架

---
+++ MLCubeAndroid/app/src/main/java/com/example/gonghe/mlcubeandroid/lcrown/mlcubeandroid/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.gonghe.mlcubeandroid.lcrown.mlcubeandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+//
+//        BottomNavigationViewEx bnve = (BottomNavigationViewEx) findViewById(R.id.bnve);
+//
+//        bnve.setIconSize(126,100);
+
+    }
+}
